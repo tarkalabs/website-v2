5d2f9c2edff63cc6f76d2b813dfcc62a57b5e730
Sudhakar <sudhakar82@gmail.com>
2020-03-09 13:51:28 +0530

    Open dev console when running and see the changes :)

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,30 @@
 // This is where it all goes :)
 
+let devMessage = `
+MMMMMMMMWXOo:,.         ..,cdOXWMMMMMMMM
+MMMMMMMMK:.                   .ckNMMMMMM
+MMMMMMMMWKl.                     ,dXMMMM
+MMMMMMMMMMWKl.                     ,kWMM
+MXddKWMMMMMMWKl.                    .oNM
+Xl  .oKWMMMMMMWKl.                    lN
+o.    .oKWMMMMMMWKl.                  .d
+.       .oKWMMMMMMWKkl.                '
+          .oKWMMMMMMMWKl.               
+            'kWMMMMMMMMW0l.             
+           .lKWMMMMMMMMMMWKl.           
+.        .lKWMMMMMMMMMMMMMMWKl.        .
+;      .lKWMMMMMMWKkkKWMMMMMMWKl.      :
+O'   .lKWMMMMMMWKl.  .lKWMMMMMMWKl.   ,0
+Wk'.lKWMMMMMMWKl.      .lKWMMMMMMWKl.,OW
+MWXXWMMMMMMWKl.          .lKWMMMMMMWXXMM
+MMMMMMMMMWKl.              .lKWMMMMMMMMM
+MMMMMMMMW0c,'''''''.         .oXMMMMMMMM
+MMMMMMMMWWNNNNNNNNXc.      .':xXMMMMMMMM
+MMMMMMMMMMMMMMMMMMWl.  ..,lkXWMMMMMMMMMM
+
+Welcome Developer!
+This is a statically generated site and I am afraid you cannot do much here.
+Why don't you connect with us (hello@tarkalabs.com) and see if we can work together?
+`
+
+console.log(devMessage);
